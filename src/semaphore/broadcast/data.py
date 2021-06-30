54f20ca32883cc43790732fd5798ad6e4ec62d83
Jonathan Sick <jsick@lsst.org>
2021-06-29 20:26:37 -0400

    Add RepeatingBroadcastMessage
    This message type uses datetime.rrule to support RFC-5545-type repeating
    events (iCal).

--- src/semaphore/broadcast/data.py
+++ src/semaphore/broadcast/data.py
@@ -8,9 +8,17 @@ from typing import TYPE_CHECKING
 import arrow
 
 if TYPE_CHECKING:
-    from typing import Optional
+    import datetime
+    from typing import Optional, Tuple
 
-__all__ = ["BroadcastMessage", "OneTimeBroadcastMessage"]
+    import dateutil.rrule
+
+
+__all__ = [
+    "BroadcastMessage",
+    "OneTimeBroadcastMessage",
+    "RepeatingBroadcastMessage",
+]
 
 
 @dataclass
@@ -48,3 +56,96 @@ class OneTimeBroadcastMessage(BroadcastMessage):
     def active(self) -> bool:
         """Whether the message should be served to clients for display."""
         return arrow.utcnow().is_between(self.defer, self.expire, bounds="[)")
+
+
+@dataclass
+class RepeatingBroadcastMessage(OneTimeBroadcastMessage):
+    """A broadcast message that repeats according to an RFC 5546 recurence
+    rule schedule.
+
+    Note
+    ----
+    The `defer` and `expire` attributes reflect either the current or the next
+    periods when the message can be displayed. These dates are refreshed
+    automatically when polling the `active` property.
+    """
+
+    rruleset: dateutil.rrule.rruleset
+    """A recurring rule set."""
+
+    ttl: datetime.timedelta
+    """The length of time the message is displayed for after each cron
+    event.
+    """
+
+    @property
+    def ttl_seconds(self) -> float:
+        return self.ttl.total_seconds()
+
+    @property
+    def active(self) -> bool:
+        """Whether the message should be served to clients for display."""
+        if self.expire < arrow.utcnow():
+            # The last message occurence expired, so let's calculate the next.
+            try:
+                candidate_defer, candidate_expire = self._propose(self.defer)
+            except ValueError:
+                return False  # no future event
+            while candidate_expire < arrow.utcnow():
+                # Keep iterating in case windows overlap
+                try:
+                    candidate_defer, candidate_expire = self._propose(
+                        candidate_defer
+                    )
+                except ValueError:
+                    return False  # no future event
+            # Set next defer and expire dates
+            self.defer = candidate_defer
+            self.expire = candidate_expire
+        return super().active
+
+    def _propose(self, after: arrow.Arrow) -> Tuple[arrow.Arrow, arrow.Arrow]:
+        """Propose a new defer/expire time after the given time"""
+        candidate_defer = self.rruleset.after(after, inc=False)
+        if candidate_defer is None:
+            raise ValueError
+        else:
+            candidate_defer = arrow.get(candidate_defer)
+        candidate_expire = candidate_defer.shift(seconds=self.ttl_seconds)
+        return (candidate_defer, candidate_expire)
+
+    @classmethod
+    def from_rruleset(
+        cls,
+        *,
+        rruleset: dateutil.rrule.rruleset,
+        ttl: datetime.timedelta,
+        summary_md: str,
+        body_md: Optional[str],
+        source_path: str,
+    ) -> RepeatingBroadcastMessage:
+        """Create a RepeatingBroadcastMessage from a recurring rule set
+        and a TTL duration for each message event.
+        """
+        # Get the next defer date from now (but rewinding by the ttl in case
+        # the message is active **right now**.
+        defer = rruleset.after(
+            arrow.utcnow().shift(seconds=-ttl.total_seconds()).datetime,
+            inc=True,
+        )
+        if defer is None:
+            raise ValueError(
+                "No future events can be schedule with this rruleset"
+            )
+        else:
+            defer = arrow.get(defer)
+        expire = defer.shift(seconds=ttl.total_seconds())
+        return cls(
+            rruleset=rruleset,
+            ttl=ttl,
+            defer=defer,
+            expire=expire,
+            summary_md=summary_md,
+            body_md=body_md,
+            source_path=source_path,
+        )
--- tests/broadcast/data_test.py
+++ tests/broadcast/data_test.py
@@ -2,9 +2,17 @@
 
 from __future__ import annotations
 
-import arrow
+import datetime
 
-from semaphore.broadcast.data import BroadcastMessage, OneTimeBroadcastMessage
+import arrow
+import pytest
+from dateutil.rrule import DAILY, HOURLY, rrule, rruleset
+
+from semaphore.broadcast.data import (
+    BroadcastMessage,
+    OneTimeBroadcastMessage,
+    RepeatingBroadcastMessage,
+)
 
 
 def test_broadcastmessage_with_body() -> None:
@@ -94,3 +102,109 @@ def test_onetimebroadcastmessage_future() -> None:
     assert m.defer == start
     assert m.expire == end
     assert m.active is False
+
+
+def test_repeating_active() -> None:
+    """Test a RepeatingBroadcastMessage that should be currently active."""
+    source_path = "broadcasts/demo.md"
+    summary = "This is the **summary** content."
+
+    start = arrow.utcnow().floor("second").shift(minutes=-10)
+    ttl = datetime.timedelta(hours=1)
+    rset = rruleset(cache=True)
+    rset.rrule(rrule(freq=DAILY, dtstart=start.datetime))
+    m = RepeatingBroadcastMessage.from_rruleset(
+        rruleset=rset,
+        ttl=ttl,
+        source_path=source_path,
+        summary_md=summary,
+        body_md=None,
+    )
+    assert m.source_path == source_path
+    assert m.summary_md == summary
+    assert m.body_md is None
+    assert m.defer == start
+    assert m.expire == start.shift(hours=1)
+    assert m.active is True
+
+
+def test_repeating_noevents_on_init() -> None:
+    """Test a RepeatingBroadcastMessage that can never be scheduled."""
+    source_path = "broadcasts/demo.md"
+    summary = "This is the **summary** content."
+
+    # No future events because of the repeat count being limited
+    start = arrow.utcnow().floor("second").shift(hours=-10)
+    ttl = datetime.timedelta(hours=1)
+    rset = rruleset(cache=True)
+    rset.rrule(rrule(freq=HOURLY, dtstart=start.datetime, count=2))
+    with pytest.raises(ValueError):
+        RepeatingBroadcastMessage.from_rruleset(
+            rruleset=rset,
+            ttl=ttl,
+            source_path=source_path,
+            summary_md=summary,
+            body_md=None,
+        )
+
+
+def test_repeating_future() -> None:
+    """Test a RepeatingBroadcastMessage that will display in the future."""
+    source_path = "broadcasts/demo.md"
+    summary = "This is the **summary** content."
+
+    # Set the start date to 10 hours ago, repeating daily with a TTL of 1 hr
+    # The next event is 1 day from now.
+    start = arrow.utcnow().floor("second").shift(hours=-10)
+    ttl = datetime.timedelta(hours=1)
+    rset = rruleset(cache=True)
+    rset.rrule(rrule(freq=DAILY, dtstart=start.datetime))
+    m = RepeatingBroadcastMessage.from_rruleset(
+        rruleset=rset,
+        ttl=ttl,
+        source_path=source_path,
+        summary_md=summary,
+        body_md=None,
+    )
+    assert m.source_path == source_path
+    assert m.summary_md == summary
+    assert m.body_md is None
+    assert m.defer == start.shift(hours=24)
+    assert m.expire == start.shift(hours=25)
+    assert m.active is False
+
+
+def test_repeating_propose_next() -> None:
+    """Test a RepeatingBroadcastMessage that needs to propose a next defer
+    time.
+    """
+    source_path = "broadcasts/demo.md"
+    summary = "This is the **summary** content."
+
+    # Build this RepeatingBroadcastMessage manually, without
+    # from_rruleset, in order to trigger a state where the last
+    # "defer" and "expire" window was in the past.
+    now = arrow.utcnow().floor("second")
+    ttl = datetime.timedelta(hours=1)
+
+    rset = rruleset(cache=True)
+    rset.rrule(rrule(freq=DAILY, dtstart=now.shift(days=-2).datetime))
+
+    orig_defer = now.shift(days=-1)
+    orig_expire = orig_defer.shift(seconds=ttl.total_seconds())
+
+    m = RepeatingBroadcastMessage(
+        rruleset=rset,
+        ttl=ttl,
+        source_path=source_path,
+        summary_md=summary,
+        body_md=None,
+        defer=orig_defer,
+        expire=orig_expire,
+    )
+    assert m.source_path == source_path
+    assert m.summary_md == summary
+    assert m.body_md is None
+    assert m.active is True
+    assert m.defer == now
+    assert m.expire == now.shift(seconds=ttl.total_seconds())
