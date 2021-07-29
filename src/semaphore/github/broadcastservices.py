25ee6e65e9d01e7e971b2583d18f2f710fae0f1d
Jonathan Sick <jsick@lsst.org>
2021-07-29 19:01:52 -0400

    Add bootstrap_broadcast_repo to run on startup
    This service runs on app startup and adds broadcast messages to the
    BroadcastMessageRepository by iterating over:
    
    - app installations
    - repositories the app is installed into
    - files in the repo's broadcasts/ directory
    
    Aside from this, the bootstrap service internally uses the same
    add_file_to_repository function that the webhook handler uses.

--- src/semaphore/github/broadcastservices.py
+++ src/semaphore/github/broadcastservices.py
@@ -4,18 +4,39 @@ from __future__ import annotations
 
 import dataclasses
 import os.path
-from typing import TYPE_CHECKING, Any, Dict, Sequence, Set, Tuple
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    AsyncIterator,
+    Dict,
+    Sequence,
+    Set,
+    Tuple,
+)
 
 from gidgethub import GitHubException, RateLimitExceeded
 from gidgethub.sansio import accept_format
 
 from semaphore.broadcast.markdown import BroadcastMarkdown
 
+from .client import (
+    create_github_client,
+    create_github_installation_client,
+    get_app_jwt,
+)
+
 if TYPE_CHECKING:
+    import httpx
     from gidgethub.httpx import GitHubAPI
     from gidgethub.sansio import Event
     from sempaphore.broadcast.repository import BroadcastMessageRepository
     from structlog.stdlib import BoundLogger
+
+
+BROADCASTS_DIR = "broadcasts"
+"""Directory relative to a GitHub repository's root that contains broadcast
+message files.
+"""
 
 
 @dataclasses.dataclass(frozen=True)
@@ -158,7 +179,7 @@ def is_broadcast_message(path: str) -> bool:
     # TODO this function could be refactored into a class containing
     # configuration about the GitHub repo, such as where messages are hosted
     # if they aren't hosted in broadcasts/
-    if os.path.dirname(path) != "broadcasts":
+    if os.path.dirname(path) != BROADCASTS_DIR:
         return False
 
     if os.path.splitext(path)[-1].lower() not in set([".md"]):
@@ -191,6 +212,9 @@ async def add_file_to_repository(
         Posix path of the message's file in GitHub.
     identifier : `GitHubMessageId`
         The file's GitHub message ID.
+    contents_url : `str`
+        The URI template for the repository's contents (the template
+        includs a ``path`` variable).
     broadcast_repo : ``BroadcastMessageRepository``
         The broadcast message repository.
     github_client : `gidgethub.httpx.GitHubAPI`
@@ -213,7 +237,7 @@ async def add_file_to_repository(
         "Downloaded broadcast from GitHub",
         message_id=dataclasses.asdict(identifier),
         github_ratelimit_remaining=(
-            github_client.ratelimit.remaining
+            github_client.rate_limit.remaining
             if github_client.rate_limit is not None
             else "Unknown"
         ),
@@ -227,3 +251,143 @@ async def add_file_to_repository(
         "Added broadcast to the repository",
         message_id=dataclasses.asdict(identifier),
     )
+
+
+async def bootstrap_broadcast_repo(
+    *,
+    http_client: httpx.AsyncClient,
+    broadcast_repo: BroadcastMessageRepository,
+    logger: BoundLogger,
+) -> None:
+    """Bootstrap data in the broadcast repo from GitHub repositories that the
+    GitHub app has access to.
+
+    Semaphore is a GitHub App that can be installed in specific repositories.
+    Semaphore's heuristic is that any repo it is installed in is eligible for
+    contributing repos.
+
+    Parameters
+    ----------
+    http_client : httpx.AsyncClient
+        The httpx client.
+    broadcast_repo : ``BroadcastMessageRepository``
+        The broadcast message repository.
+    logger
+        The logger instance.
+    """
+    github_client = create_github_client(http_client=http_client)
+    jwt = get_app_jwt()
+    async for installation in iter_installations(
+        github_client=github_client, jwt=jwt
+    ):
+        installation_client = await create_github_installation_client(
+            http_client=http_client, installation_id=installation["id"]
+        )
+        async for github_repo in iter_installation_repositories(
+            github_client=installation_client
+        ):
+            logger.debug("interating on github_repo", github_repo=github_repo)
+            async for file_obj in iter_repo_dir_contents(
+                github_client=installation_client,
+                contents_url=github_repo["contents_url"],
+                directory_path=BROADCASTS_DIR,
+            ):
+                if is_broadcast_message(file_obj["path"]):
+                    await add_file_to_repository(
+                        file_path=file_obj["path"],
+                        identifier=GitHubMessageId(
+                            path=file_obj["path"],
+                            repo_name=github_repo["name"],
+                            repo_owner=github_repo["owner"]["login"],
+                            ref=f'refs/heads/{github_repo["default_branch"]}',
+                        ),
+                        contents_url=github_repo["contents_url"],
+                        broadcast_repo=broadcast_repo,
+                        github_client=installation_client,
+                        logger=logger,
+                    )
+
+
+async def iter_installations(
+    *, github_client: GitHubAPI, jwt: str
+) -> AsyncIterator[Dict[str, Any]]:
+    """Iterate over the GitHub app installations.
+
+    Parameters
+    ----------
+    github_client : `gidgethub.httpx.GitHubAPI`
+        The GitHub client.
+    jwt : `str`
+        The JWT for the GitHub application. See `get_app_jwt`.
+
+    Yields
+    ------
+    dict
+        The installation resource from the GitHub v3 API. See
+        https://docs.github.com/en/rest/reference/apps#list-installations-for-the-authenticated-app
+
+        The app's installation id in the ``id`` key at the root.
+    """
+    url = "/app/installations"
+    async for inst in github_client.getiter(url, jwt=jwt):
+        yield inst
+
+
+async def iter_installation_repositories(
+    *,
+    github_client: GitHubAPI,
+) -> AsyncIterator[Dict[str, Any]]:
+    """Iterate over repositories that the GitHub app installation has access
+    to.
+
+    Parameters
+    ----------
+    github_client : `gidgethub.httpx.GitHubAPI`
+        The GitHub client that is pre-authorized with an installation ID,
+        see `create_github_installation_client`.
+
+    Yields
+    ------
+    dict
+        The repository resource from the GitHub v3 API. See
+        https://docs.github.com/en/rest/reference/apps#list-repositories-accessible-to-the-app-installation
+    """
+    # FIXME we're temporarily getitem instead of getiter because
+    # of https://github.com/brettcannon/gidgethub/issues/164
+    # this means we'll miss any extra pages, but that should be fine for now
+    # given the small number of installations expected (generally 1 repo!).
+    data = await github_client.getitem("/installation/repositories")
+    for repo in data["repositories"]:
+        yield repo
+
+
+async def iter_repo_dir_contents(
+    github_client: GitHubAPI, contents_url: str, directory_path: str
+) -> AsyncIterator[Dict[str, Any]]:
+    """Iterate over content objects in a repository in a GitHub repository.
+
+    Parameters
+    ----------
+    github_client : `gidgethub.httpx.GitHubAPI`
+        The GitHub client that is pre-authorized with an installation ID,
+        see `create_github_installation_client`.
+    contents_url : `str`
+        The contents URL for the repository, which is a templated URI,
+        Example:
+        ``https://api.github.com/repos/lsst-sqre/semaphore-demo/contents/{+path}``
+    directory_path : `str`
+        The directory path to iterate in.
+
+    Yields
+    ------
+    dict
+        The repository content resource. See
+        https://docs.github.com/en/rest/reference/repos#contents
+    """
+    if directory_path.endswith("/"):
+        directory_path = directory_path.rstrip("/")
+
+    async for file_obj in github_client.getiter(
+        contents_url, url_vars={"path": directory_path}
+    ):
+        yield file_obj
--- src/semaphore/main.py
+++ src/semaphore/main.py
@@ -4,12 +4,15 @@ from __future__ import annotations
 
 from importlib.metadata import metadata
 
+import structlog
 from fastapi import FastAPI
 from safir.dependencies.http_client import http_client_dependency
 from safir.logging import configure_logging
 from safir.middleware.x_forwarded import XForwardedMiddleware
 
 from .config import config
+from .dependencies.broadcastrepo import broadcast_repo_dependency
+from .github.broadcastservices import bootstrap_broadcast_repo
 from .handlers.external import external_router
 from .handlers.internal import internal_router
 
@@ -32,7 +35,17 @@ app.include_router(external_router)
 
 @app.on_event("startup")
 async def startup_event() -> None:
+    logger = structlog.get_logger(config.logger_name)
+    logger.info("Running startup")
+
     app.add_middleware(XForwardedMiddleware)
+
+    if config.enable_github_app:
+        await bootstrap_broadcast_repo(
+            http_client=http_client_dependency(),
+            broadcast_repo=broadcast_repo_dependency(),
+            logger=logger,
+        )
 
 
 @app.on_event("shutdown")
